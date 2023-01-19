a6531b6f63a7ce55b1528634f5e09faaf65eaccd
Khalil Naji <najiahm@sheridancollege.ca>
2023-01-19 15:27:30 -0500

    comments added

---
+++ src/gitdemo/GitDemo.java
@@ -0,0 +1,16 @@
+package gitdemo;
+
+/**
+ * This class +++ Insert class description here+++
+ * @author Khalil
+ */
+public class GitDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
